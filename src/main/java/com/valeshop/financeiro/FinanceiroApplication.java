1111cc5ceb38a78ead11b0b74b732442b9babc7a
PedroRomaoDev <pedro7ntj@gmail.com>
2026-09-16 15:47:36 -0300

    feat: initialize Spring Boot application and gitignore

---
+++ src/main/java/com/valeshop/financeiro/FinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.valeshop.financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceiroApplication.class, args);
+    }
+
+}
